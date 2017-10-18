8d9f075b422eee51e31874f85419e885bf5ae584
AlexFdez10 <alexandro_fb_97@hotmail.com>
2017-10-18 12:47:09 +0200

    Initial commit

---
+++ app/src/main/java/com/example/dell/splashscreen/SplashScreenActivity.java
@@ -0,0 +1,26 @@
+package com.example.dell.splashscreen;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Lanzaremos despues de 3000ms(3s), el MainActivity
+                Intent intent=new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish(); //Para que si vuelvo atras no vuelva a aparecer la splashscreen, sino que directamente cierre la aplicacion
+
+            }
+        }, 3000);
+    }
+}
